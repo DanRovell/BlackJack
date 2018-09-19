09e6178c7ecde3b6056062acc93688681da6d6b3
Daniel Rovell <danrovell@gmail.com>
2018-09-18 19:21:39 -0500

    changes made during interview

--- blackjack.py
+++ blackjack.py
@@ -13,16 +13,18 @@ import sys
 import datetime
 
 values = {'ace': 11, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'Jack': 10, 'Queen': 10, 'King': 10}
-
+# 
 #Function for making a deck
 def newDeck(values):
     deck = []
     suits = ['hearts', 'diamonds', 'spades', 'clubs']
+    
     index = 0
     for key in values:
         for f in range(0, len(suits)):
             deck.append(key + ' of ' + suits[f])
             index += 1
+    random.shuffle(deck)
     return deck
 
 #Player class, each player has a hand
@@ -34,10 +36,30 @@ class Player:
         draw = random.choice(deck) 
         return draw
 
+# class Game:
+#     def __init__()
+
+#     self.playing = True
+#     self.betting = False
+#     self.firstRound = True
+#     self.bet = 0
+#     self.playerTotal = 0
+#     self.dealerTotal = 0
+#     self.playerWonTotal = 0
+#     self.playerWon = 0
+#     self.playerLostTotal = 0
+#     self.playerLost = 0
+#     self.playerMoney = 0
+#     self.dealerMoney = 0
+#     self.playerLosses = 0
+#     self.playerWins = 0
+
+
 #Computer the score and run an ace check
 def computeScore(hand):
     total = 0
     value_set = []
+    print(hand)
     for i in hand:
         card = i.partition(' ')[0]
         if card in values:
@@ -45,14 +67,56 @@ def computeScore(hand):
             value_set.append(x)
     total = (sum(value_set))
     ace_check = True
+    
     while ace_check == True:
-        if total > 21 and 11 in value_set:
-            value_set.remove(11)
-            value_set.append(1)
-            total = sum(value_set)
-        else:
-            ace_check = False
+        for i in hand:
+            card = i.partition(' ')[0]
+            if card == 'ace':
+                if total > 21:
+                    value_set.remove(11)
+                    value_set.append(1)
+                    total = sum(value_set)
+                else:
+                    ace_check = False
+            else:
+                ace_check = False 
     return total 
+
+def yourTurn(status, player, deck, playerTotal):
+    while (status == 'yourTurn'):
+        choice = input('Will you hit or stay? ')
+        if choice == 'hit':
+            player.hand.append(deck.pop())
+            playerTotal = computeScore(player.hand,)              
+            print('Your hand is now: ' + ', '.join(player.hand))
+            print('Your Hand Total: {0}'.format(playerTotal))
+            if playerTotal > 21:
+                status = 'youLose'
+            else: status = 'yourTurn'
+        elif choice == 'stay':
+            status = 'dealersTurn'
+        else:
+            print('Please enter \'hit\' or \'stay\'')
+    
+    return status
+
+def dealersTurn(status, deck, dealer, dealerTotal, playerTotal):
+    while (status == 'dealersTurn'):
+            if (dealerTotal >= 17):
+                if dealerTotal >21 or (dealerTotal <= playerTotal):
+                    status = 'youWin'
+                else:
+                    status = 'youLose'
+            elif(dealerTotal < 17):
+                dealer.hand.append(deck.pop())
+                dealerTotal = computeScore(dealer.hand)
+                print('The dealer\'s hand is: ' + ', '.join(dealer.hand))
+                print('Dealer\'s Hand Total: {0}'.format(dealerTotal))
+                status = 'dealersTurn'
+    
+    return status
+
+    
 
 #Main function
 def main():
@@ -81,11 +145,14 @@ def main():
     #Game loop
     while playing == True:
         #Initialize deck, player, and dealer
-        deck = newDeck(values)
+        if firstRound == True:
+            deck = newDeck(values)
         player = Player([],[])
         dealer = Player([],[])
 
         #Will the player be betting money?
+        if len(deck) < 20:
+            deck = newDeck(values)
         status = 'checkBetting'
         while (status == 'checkBetting'):
             if (firstRound == False) and (playerMoney == 0):
@@ -130,54 +197,25 @@ def main():
                     print('Player: ${0}'.format(playerMoney))
                     print('Dealer: ${0}'.format(dealerMoney))
                     break
-            player.hand.append(player.drawCard(deck))
-            deck.remove(player.hand[0])
-            player.hand.append(player.drawCard(deck))
+            
+            player.hand.append(deck.pop())
+            player.hand.append(deck.pop())
             playerTotal = computeScore(player.hand)
-            deck.remove(player.hand[1])
             print('You were dealt: ' + ', '.join(player.hand))
             print('Your Hand Total: {0}'.format(playerTotal))
-            dealer.hand.append(player.drawCard(deck))
+            dealer.hand.append(deck.pop())
             dealerTotal = computeScore(dealer.hand)
-            deck.remove(dealer.hand[0])
             print('The dealer\'s hand is: ' + ', '.join(dealer.hand))
             print('Dealer\'s Hand Total: {0}'.format(dealerTotal))
             status = 'yourTurn'
-        
         #Player Decides to hit or stay
-        while (status == 'yourTurn'):
-            choice = input('Will you hit or stay? ')
-            if choice == 'hit':
-                player.hand.append(player.drawCard(deck))
-                playerTotal = computeScore(player.hand,)              
-                deck.remove(player.hand[-1])
-                print('Your hand is now: ' + ', '.join(player.hand))
-                print('Your Hand Total: {0}'.format(playerTotal))
-                if playerTotal > 21:
-                    status = 'youLose'
-                else: status = 'yourTurn'
-            elif choice == 'stay':
-                status = 'dealersTurn'
-            else:
-                print('Please enter \'hit\' or \'stay\'')
+        status = yourTurn(status, player, deck, playerTotal)
 
         #After player stays, dealer's turn 
-        while (status == 'dealersTurn'):
-            if (dealerTotal >= 17):
-                if dealerTotal >21 or (dealerTotal <= playerTotal):
-                    status = 'youWin'
-                else:
-                    status = 'youLose'
-            elif(dealerTotal < 17):
-                dealer.hand.append(player.drawCard(deck))
-                dealerTotal = computeScore(dealer.hand)
-                deck.remove(dealer.hand[-1])
-                print('The dealer\'s hand is: ' + ', '.join(dealer.hand))
-                print('Dealer\'s Hand Total: {0}'.format(dealerTotal))
-                status = 'dealersTurn'
+        status = dealersTurn(status, deck, dealer, dealerTotal, playerTotal)
 
         #After player wins
-        while status == 'youWin':
+        if status == 'youWin':
             print('You Win')
             playerWins += 1
             playerWon += bet
@@ -188,7 +226,7 @@ def main():
             status = 'playAgain'
 
         #After player loses
-        while status == 'youLose':
+        if status == 'youLose':
             print('You Lose')
             playerLosses += 1
             playerLost += bet
